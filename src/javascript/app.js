7c809f1789097a69893d04d6a1a6ac253065a35e
aurelien.hmdy <aurelien@hemidy.fr>
2022-10-25 14:48:38 +0200

    :sparkles: Added gallery link with anim

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -9,13 +9,19 @@ import '../scss/app.scss';
 
 class App {
   constructor() {
+    this.isOneHeroSectionActive = false;
     this.heroSection = document.querySelector('.heroContainer');
     this.heroSectionImage = document.querySelectorAll('.heroContainer__genreSection');
+    // this.heroSectionGalleryLink = document.querySelectorAll('.heroContainer__genreSection--gallery-link');
 
     this.heroSectionImage.forEach((image) => {
       // Add onCLick element event
       image.addEventListener('click', (e) => {
-        console.log('clicked');
+        if (this.isOneHeroSectionActive) return;
+        this.isOneHeroSectionActive = true;
+        // if (e.target.classList.contains('heroContainer__genreSection--gallery-link')) return;
+
+        console.log(e.target);
         // Remove the active class of the previous clicked element
         this.heroSectionImage.forEach((image) => {
           image.classList.remove('active');
@@ -32,6 +38,8 @@ class App {
     window.addEventListener('click', (e) => {
       // Check if on parent of element has the following class
       if (e.target.closest('.heroContainer')) return;
+
+      this.isOneHeroSectionActive = false;
 
       this.heroSectionImage.forEach((image) => {
         image.classList.remove('active');
