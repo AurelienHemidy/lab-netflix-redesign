650db5795ae8e6ce24d31b36b89ae704ab904d65
aurelien.hmdy <aurelien@hemidy.fr>
2022-10-25 11:43:51 +0200

    :sparkles: Added entry animation on click

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -7,72 +7,26 @@ import '../scss/app.scss';
  * Javascript
  */
 
-// Loaders
-import ResourceLoader from './vendor/loaders/ResourceLoader';
-import ThreeGLTFLoader from './vendor/loaders/loaders/three-gltf-loader';
-import ImageLoader from './vendor/loaders/loaders/image-loader';
-import LoaderUI from './vendor/loaders/LoaderUI';
-
-// Resources
-import globalResources from '../../static/datas/globalResources';
-
-// Utils
-import bindAll from './utils/bindAll';
-
 class App {
   constructor() {
-    // this._resourceLoader = new ResourceLoader();
-    // this.loaderUI = new LoaderUI('loader');
-    // this.datas = null;
-    // this._bindAll();
-    // this.registerLoaders();
-    // this.setupResources();
-    // this.setupEventListeners();
-    // this.loadResources();
-
-    this.heroSection = document.querySelectorAll('.heroContainer__genreSection');
+    this.heroSection = document.querySelector('.heroContainer');
+    this.heroSectionImage = document.querySelectorAll('.heroContainer__genreSection');
     console.log(this.heroSection);
 
-    this.heroSection.forEach((section) => {
-      section.addEventListener('click', (e) => {
-        console.log(e.target);
+    this.heroSectionImage.forEach((image) => {
+      // Add onCLick element event
+      image.addEventListener('click', (e) => {
+        // Remove the active class of the previous clicked element
+        this.heroSectionImage.forEach((image) => {
+          image.classList.remove('active');
+          image.classList.add('disabled');
+        });
+
+        // Add the active class to the current clicked element
+        e.target.parentElement.classList.remove('disabled');
+        e.target.parentElement.classList.add('active');
       });
     });
-  }
-
-  registerLoaders() {
-    this._resourceLoader.registerLoader({ loader: ThreeGLTFLoader, type: 'gltf' });
-    this._resourceLoader.registerLoader({ loader: ImageLoader, type: 'jpg' });
-  }
-
-  setupResources() {
-    this._resourceLoader.add({
-      resources: globalResources,
-      preload: true,
-    });
-  }
-
-  loadResources() {
-    this._resourceLoader.preload();
-  }
-
-  setupEventListeners() {
-    this._resourceLoader.addEventListener('complete', this._loadResourcesCompleteHandler);
-    this._resourceLoader.addEventListener('progress', this._loadResourcesProgressHandler);
-  }
-
-  _loadResourcesCompleteHandler(e) {
-    // console.log("loaded");
-    this.datas = e;
-    this.loaderUI.toggleLoader();
-  }
-
-  _bindAll() {
-    bindAll(this, '_loadResourcesProgressHandler', '_loadResourcesCompleteHandler');
-  }
-
-  _loadResourcesProgressHandler(e) {
-    this.loaderUI.setLoaderProgress(Math.round(e * 100));
   }
 }
 
