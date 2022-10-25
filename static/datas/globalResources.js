daa1d83726e4ce11a772e91bbc46a8ed01740189
aurelien.hmdy <aurelien@hemidy.fr>
2022-10-25 18:34:45 +0200

    :bento: Added images for gallery

--- static/datas/globalResources.js
+++ static/datas/globalResources.js
@@ -1,78 +1,33 @@
 const textures = [
-    // {
-    //     name: "test",
-    //     type: "texture",
-    //     path: "https://picsum.photos/200/300",
-    // }
+  // {
+  //     name: "test",
+  //     type: "texture",
+  //     path: "https://picsum.photos/200/300",
+  // }
 ];
 
 const models = [
-    {
-        name: 'Duck',
-        type: 'gltf',
-        path: '/models/Duck/Duck.gltf',
-    },
-    // {
-    //     name: 'Fox',
-    //     type: 'gltf',
-    //     path: '/models/Fox/Fox.gltf',
-    // },
-    // {
-    //     name: 'FlightHelmet',
-    //     type: 'gltf',
-    //     path: '/models//FlightHelmet/FlightHelmet.gltf',
-    // },
-    // {
-    //     name: 'FlightHelmet',
-    //     type: 'gltf',
-    //     path: '/models//FlightHelmet/FlightHelmet.gltf',
-    // },
-    // {
-    //     name: 'FlightHelmet',
-    //     type: 'gltf',
-    //     path: '/models//FlightHelmet/FlightHelmet.gltf',
-    // },
-    // {
-    //     name: 'FlightHelmet',
-    //     type: 'gltf',
-    //     path: '/models//FlightHelmet/FlightHelmet.gltf',
-    // },
-    // {
-    //     name: 'FlightHelmet',
-    //     type: 'gltf',
-    //     path: '/models//FlightHelmet/FlightHelmet.gltf',
-    // },
-    // {
-    //     name: 'FlightHelmet',
-    //     type: 'gltf',
-    //     path: '/models//FlightHelmet/FlightHelmet.gltf',
-    // },
-    // {
-    //     name: 'FlightHelmet',
-    //     type: 'gltf',
-    //     path: '/models//FlightHelmet/FlightHelmet.gltf',
-    // },
-    // {
-    //     name: 'FlightHelmet',
-    //     type: 'gltf',
-    //     path: '/models//FlightHelmet/FlightHelmet.gltf',
-    // },
+  {
+    name: 'Duck',
+    type: 'gltf',
+    path: '/models/Duck/Duck.gltf',
+  },
 ];
 
 const sounds = [
-    // {
-    //     name: 'test-sound',
-    //     type: 'audio',
-    //     path: '/sounds/audio.mp3',
-    // }
+  // {
+  //     name: 'test-sound',
+  //     type: 'audio',
+  //     path: '/sounds/audio.mp3',
+  // }
 ];
 
 const images = [
-   {
-       name: 'image',
-       type: 'jpg',
-       path: '/images/image.jpg'
-   } 
+  {
+    name: 'image',
+    type: 'jpg',
+    path: '/images/image.jpg',
+  },
 ];
 
 export default [...textures, ...models, ...sounds, ...images];
---
+++ static/datas/imagesSourcesGrid.js
@@ -0,0 +1,58 @@
+const images = [
+  {
+    url: '/images/gallery/interstellar/interstellar-1.jpg',
+    gridArea: {
+      gridRowStart: '10',
+      gridColumnStart: '29',
+      gridRowEnd: '24',
+      gridColumnEnd: '41',
+    },
+  },
+  {
+    url: '/images/gallery/interstellar/interstellar-2.jpg',
+    gridArea: {
+      gridRowStart: '35',
+      gridColumnStart: '22',
+      gridRowEnd: '46',
+      gridColumnEnd: '32',
+    },
+  },
+  {
+    url: '/images/gallery/interstellar/interstellar-3.jpg',
+    gridArea: {
+      gridRowStart: '30',
+      gridColumnStart: '45',
+      gridRowEnd: '38',
+      gridColumnEnd: '51',
+    },
+  },
+  {
+    url: '/images/gallery/interstellar/interstellar-4.jpg',
+    gridArea: {
+      gridRowStart: '17',
+      gridColumnStart: '6',
+      gridRowEnd: '42',
+      gridColumnEnd: '14',
+    },
+  },
+  {
+    url: '/images/gallery/interstellar/interstellar-5.jpg',
+    gridArea: {
+      gridRowStart: '4',
+      gridColumnStart: '1',
+      gridRowEnd: '11',
+      gridColumnEnd: '7',
+    },
+  },
+  {
+    url: '/images/gallery/interstellar/interstellar-6.jpg',
+    gridArea: {
+      gridRowStart: '39',
+      gridColumnStart: '3',
+      gridRowEnd: '46',
+      gridColumnEnd: '9',
+    },
+  },
+];
+
+export default images;
